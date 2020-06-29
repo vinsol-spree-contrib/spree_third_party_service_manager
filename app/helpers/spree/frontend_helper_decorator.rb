e2f02310ae1ff692cd19d43741cb0022604ec212
rishabh gupta <rishabh@vinsol.com>
2020-06-29 13:06:55 +0530

    changed helper

--- app/helpers/spree/frontend_helper_decorator.rb
+++ app/helpers/spree/frontend_helper_decorator.rb
@@ -1,8 +1,10 @@
 module Spree
-  module FrontendHelper
+  module FrontendHelperDecorator
     def current_page(controller, action)
       page_name = controller + '/' + action
       @current_page ||= Spree::Page.find_by_name(page_name)
     end
   end
 end
+
+Spree::FrontendHelper.prepend Spree::FrontendHelperDecorator
