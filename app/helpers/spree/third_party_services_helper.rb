733b7844a3e73a30f557717f92ac7ca1a66a6c24
rishabh gupta <rishabh@vinsol.com>
2020-07-15 13:01:46 +0530

    changed helper name

--- app/helpers/spree/third_party_services_helper.rb
+++ app/helpers/spree/third_party_services_helper.rb
@@ -1,10 +1,8 @@
 module Spree
-  module FrontendHelperDecorator
+  module ThirdPartyServicesHelper
     def current_page(controller, action)
       page_name = controller + '/' + action
       @current_page ||= Spree::Page.find_by_name(page_name)
     end
   end
 end
-
-Spree::FrontendHelper.prepend Spree::FrontendHelperDecorator
--- app/overrides/add_third_party_services_scripts_to_frontend_head.rb
+++ app/overrides/add_third_party_services_scripts_to_frontend_head.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(virtual_path: 'spree/layouts/spree_application',
                      name: 'add_third_party_services_scripts_to_frontend_head',
                      insert_bottom: '[data-hook="inside_head"]',
-                     partial: 'spree/shared/third_party_services_scripts'
+                     partial: 'spree/shared/third_party_services/third_party_services_scripts'
                      )
