cdc44fb4db04dc83381d5dac87f60b8aee9daec5
Naresh Sharma <naresh@vinsol.com>
2020-12-02 18:58:33 +0530

    taken out virtual_path and action selector

--- app/overrides/add_third_party_service_manager_to_admin_tabs.rb
+++ app/overrides/add_third_party_service_manager_to_admin_tabs.rb
@@ -1,19 +1,16 @@
+file_path = 'spree/layouts/admin'
+deface_selector = "[data-hook='admin_tabs'] erb[loud]:contains('spree/admin/shared/main_menu')"
 if Spree.version.to_f >= 4.0
-  Deface::Override.new(virtual_path: 'spree/admin/shared/_main_menu',
-                       name: 'add_third_party_service_manager_to_admin_tabs',
-                       insert_after: "erb[loud]:contains('t(:configurations)')",
-                       text: '<ul class="nav nav-sidebar border-top w-100">
-                                <%= main_menu_tree Spree.t(:third_party_services), icon: "cloud", sub_menu: "third_party_services", url: "#sidebar-third-party-services" %>
-                              </ul>'
-                      )
-else
-  Deface::Override.new(virtual_path: 'spree/layouts/admin',
+  file_path = 'spree/admin/shared/_main_menu'
+  deface_selector = "erb[loud]:contains('t(:configurations)')"
+end
+
+Deface::Override.new(virtual_path: file_path,
                      name: 'add_third_party_service_manager_to_admin_tabs',
-                     insert_after: "[data-hook='admin_tabs'] erb[loud]:contains('spree/admin/shared/main_menu')",
-                     text: '<ul class="nav nav-sidebar">
+                     insert_after: deface_selector,
+                     text: '<ul class="nav nav-sidebar border-top w-100">
                               <%= main_menu_tree Spree.t(:third_party_services), icon: "cloud", sub_menu: "third_party_services", url: "#sidebar-third-party-services" %>
                             </ul>'
                     )
-end
 
 
