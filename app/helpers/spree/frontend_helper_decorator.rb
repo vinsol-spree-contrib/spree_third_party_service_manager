7469ba8ee82099be162dfad1d3103e764c95cac9
Naresh Sharma <naresh@vinsol.com>
2020-12-04 14:02:30 +0530

    update module name

--- app/helpers/spree/frontend_helper_decorator.rb
+++ app/helpers/spree/frontend_helper_decorator.rb
@@ -1,4 +1,4 @@
-Spree::FrontendHelper.module_eval do
+module Spree::FrontendHelper
   def current_page(controller, action)
     page_name = controller + '/' + action
     @current_page ||= Spree::Page.find_by_name(page_name)
