303405d8065bc6e041dc481c04aca44388b509e6
Naresh Sharma <naresh@vinsol.com>
2020-12-07 15:59:43 +0530

    created third party helper

--- app/helpers/spree/third_party_helper.rb
+++ app/helpers/spree/third_party_helper.rb
@@ -1,6 +1,8 @@
-module Spree::FrontendHelper
+module Spree::ThirdPartyHelper
   def current_page(controller, action)
     page_name = controller + '/' + action
     @current_page ||= Spree::Page.find_by_name(page_name)
   end
+
+  module_function :current_page
 end
